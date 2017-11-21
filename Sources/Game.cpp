0f9c0ee4a82590d5f59483054a0062e1f3d5c598
hw16a129 <hw16a129@oecu.jp>
2017-11-21 16:15:29 +0900

    GとHを追加しました

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -7,8 +7,8 @@
 // TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
 // TODO: スコアのサイズを大きくする。(E)
 // TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
-// TODO: PlayBGM()関数を使って、BGMを再生する。(G)
-// TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
+// TODO: PlayBGM()関数を使って、BGMを再生する。(G HW16A129 出口友香)
+// TODO: PlaySE()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H HW16A129 出口友香)
 
 
 Vector2 cloudPos;       //!< 雲の位置
@@ -26,6 +26,8 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    
+    PlayBGM("bgm_maoudamashii_8bit07.mp3");
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -34,6 +36,8 @@ void Update()
     // 弾の発射
     if (bulletPos.x <= -999 && Input::GetKeyDown(KeyMask::Space)) {
         bulletPos = cannonPos + Vector2(50, 10);
+        
+        PlaySound("se_maoudamashii_explosion03.mp3");
     }
 
     // 弾の移動
@@ -45,6 +49,8 @@ void Update()
         if (targetRect.Overlaps(bulletRect)) {
             score += 1;         // スコアの加算
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
+            
+            PlaySound("se_maoudamashii_explosion06.mp3");
         }
     }
 
